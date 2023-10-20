60fd1241d20e016fc96b58e772a2ee658d9ec16d
SurajGudaji <Suraj.Gudaji1@ibm.com>
2023-10-20 19:33:43 +0530

    Added function to check cluster deploy status and made minor improvements in other functions

--- monitor.py
+++ monitor.py
@@ -8,57 +8,88 @@ from datetime import datetime
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-url_set = ['']
-
 def get_current_date():
     return datetime.now().date()
 
 def parse_build_date(da):
-    date_2=datetime.strptime(da,"%Y-%m-%dT%H:%M:%SZ")
-    job_time=date_2.date()
-    return job_time
+    parse_date=datetime.strptime(da,"%Y-%m-%dT%H:%M:%SZ")
+    job_run_date=parse_date.date()
+    return job_run_date
 
 def get_builds(s):
-    # print("Hi")
-    for url in s:
-        print("----------------------------------------------------------------------------------------------")
-        response = requests.get(url, verify=False)
-        # print(response.status_code)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            script_elements = soup.find_all('script')
-            selected_script_element = None
-            # print(len(script_elements))
-            for script_element in script_elements:
-                script_content = script_element.string
-                if script_content:
-                    if 'allBuilds' in script_content:
-                        selected_script_element = script_content
-                        break
-            if selected_script_element:
-                var_name = 'allBuilds'
-                pattern = rf'{var_name}\s*=\s*(.*?);'
-                match = re.search(pattern, selected_script_element)
-                if match:
-                    all_value=match.group(1)
-                    try:
-                        all_array=json.loads(all_value)
-                        current_date=get_current_date()
-                        for ele in all_array:
-                            job_time=parse_build_date(ele["Started"])
-                            if job_time == current_date:
-                                print(ele["ID"],ele["Result"])
-                    except json.JSONDecodeError as e:
-                        print("convert failed")
-        else:
-            print("response failed")
-
-# def get_failure_type():
-#     response = requests.get('https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/periodic-ci-openshift-multiarch-master-nightly-4.15-upgrade-from-nightly-4.14-ocp-ovn-remote-libvirt-ppc64le/1715201290650259456/artifacts/ocp-ovn-remote-libvirt-ppc64le/ipi-install-libvirt-install/finished.json', verify=False)
-#     if response.status_code == 200:
-#         print(response.text)
-
-# get_failure_type()
-get_builds(url_set)
-
     
+    print("----------------------------------------------------------------------------------------------")
+    response = requests.get(s, verify=False)
+    # print(response.text)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_elements = soup.find_all('script')
+        selected_script_element = None
+
+        for script_element in script_elements:
+            script_content = script_element.string
+            if script_content:
+                if 'allBuilds' in script_content:
+                    selected_script_element = script_content
+                    break
+        
+        if selected_script_element:
+            var_name = 'allBuilds'
+            pattern = rf'{var_name}\s*=\s*(.*?);'
+
+            match = re.search(pattern, selected_script_element)
+            if match:
+                all_builds=match.group(1)
+                # print(all_builds)
+                try:
+                    all_builds_parsed=json.loads(all_builds)
+                    current_date=get_current_date()
+                    build_list = []
+                    for ele in all_builds_parsed:
+                        job_time=parse_build_date(ele["Started"])
+                        if job_time == current_date and ele["Result"] != "PENDING":
+                            job_id = ele["ID"]
+                            job_log_path = ele["SpyglassLink"]
+                            job_dict = {"ID": job_id, "SpyglassLink": job_log_path}
+                            build_list.append(job_dict)
+                    return build_list
+                except json.JSONDecodeError as e:
+                    print("convert failed")
+    else:
+        print("response failed")
+    
+    #work on removing the unwanted code used to select the script
+    #improve code to do better error handling
+
+def cluster_deploy_status(spy_link):
+    job_type,job_platform = job_classifier(spy_link)
+    job_log_url = 'https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs' + spy_link[8:] + '/artifacts/' + job_type + '/ipi-install-' + job_platform +'-install/finished.json'
+    response = requests.get(job_log_url, verify=False)
+    cluster_status = json.loads(response.text)
+    return cluster_status["result"]
+    #next implemlent functions to check for cluster creartion errors and e2e errors and call respective function to analyze the errors
+
+def job_classifier(spy_link):
+    if spy_link.find("powervs") != -1:
+        job_platform = "powervs"
+        job_type = "ocp-e2e-ovn-ppc64le-powervs"
+        return job_type,job_platform
+    elif spy_link.find("libvirt") != -1:
+        job_platform = "libvirt"
+        if spy_link.find("upgrade") != -1:
+            job_type='ocp-ovn-remote-libvirt-ppc64le'
+        else:
+            job_type='ocp-e2e-ovn-remote-libvirt-ppc64le'
+        return job_type,job_platform
+    else:
+        return 1
+        
+def temporary_main_function():
+    url_set = ['']    
+    for url in url_set:
+        build_list = get_builds(url) 
+        for build in build_list:
+            cluster_status=cluster_deploy_status(build["SpyglassLink"])
+            print(build,cluster_status)
+
+temporary_main_function()    
