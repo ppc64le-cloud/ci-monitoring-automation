752d10d9a7b154f3d62f198d5961c36f375f6299
SurajGudaji <Suraj.Gudaji1@ibm.com>
2023-10-23 17:00:12 +0530

    added function to get list of failures and renamed variables

--- monitor.py
+++ monitor.py
@@ -11,12 +11,12 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def get_current_date():
     return datetime.now().date()
 
-def parse_build_date(da):
+def parse_job_date(da):
     parse_date=datetime.strptime(da,"%Y-%m-%dT%H:%M:%SZ")
     job_run_date=parse_date.date()
     return job_run_date
 
-def get_builds(s):
+def get_jobs(s):
     
     print("----------------------------------------------------------------------------------------------")
     response = requests.get(s, verify=False)
@@ -39,20 +39,20 @@ def get_builds(s):
 
             match = re.search(pattern, selected_script_element)
             if match:
-                all_builds=match.group(1)
+                all_jobs=match.group(1)
                 # print(all_builds)
                 try:
-                    all_builds_parsed=json.loads(all_builds)
+                    all_jobs_parsed=json.loads(all_jobs)
                     current_date=get_current_date()
-                    build_list = []
-                    for ele in all_builds_parsed:
-                        job_time=parse_build_date(ele["Started"])
+                    jobs_run_today = []
+                    for ele in all_jobs_parsed:
+                        job_time=parse_job_date(ele["Started"])
                         if job_time == current_date and ele["Result"] != "PENDING":
                             job_id = ele["ID"]
                             job_log_path = ele["SpyglassLink"]
                             job_dict = {"ID": job_id, "SpyglassLink": job_log_path}
-                            build_list.append(job_dict)
-                    return build_list
+                            jobs_run_today.append(job_dict)
+                    return jobs_run_today
                 except json.JSONDecodeError as e:
                     print("convert failed")
     else:
@@ -67,7 +67,6 @@ def cluster_deploy_status(spy_link):
     response = requests.get(job_log_url, verify=False)
     cluster_status = json.loads(response.text)
     return cluster_status["result"]
-    #next implemlent functions to check for cluster creartion errors and e2e errors and call respective function to analyze the errors
 
 def job_classifier(spy_link):
     if spy_link.find("powervs") != -1:
@@ -83,13 +82,44 @@ def job_classifier(spy_link):
         return job_type,job_platform
     else:
         return 1
-        
-def temporary_main_function():
-    url_set = ['']    
-    for url in url_set:
-        build_list = get_builds(url) 
-        for build in build_list:
-            cluster_status=cluster_deploy_status(build["SpyglassLink"])
-            print(build,cluster_status)
 
-temporary_main_function()    
+def get_failed_e2e_testcases(spy_link,job_type):
+
+    test_log_junit_dir_url = "https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs" + spy_link[8:] + "/artifacts/" + job_type + "/openshift-e2e-libvirt-test/artifacts/junit/"
+
+    response = requests.get(test_log_junit_dir_url, verify=False)
+    if response.status_code == 200:
+        test_failure_summary_filename_re = re.compile('(test-failures-summary_2[^.]*\.json)')
+        test_failure_summary_filename_match = test_failure_summary_filename_re.search(response.text, re.MULTILINE|re.DOTALL)
+        if test_failure_summary_filename_match is not None:
+            test_failure_summary_filename_str = test_failure_summary_filename_match.group(1)
+            test_log_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs" + spy_link[8:] + "/artifacts/" + job_type + "/openshift-e2e-libvirt-test/artifacts/junit/" + test_failure_summary_filename_str
+            response_2 = requests.get(test_log_url,verify=False)
+            if response_2.status_code == 200:
+                data = response_2.json()
+                e2e_failure_list = data['Tests']
+                return e2e_failure_list
+            else:
+                print("error parsing data")
+        else:
+            print("ERROR: Could not find test-failures-summary_*.json?")
+    else:
+        print("error")
+
+
+def temporary_main_function():
+    url_set = ['https://prow.ci.openshift.org/job-history/gs/origin-ci-test/logs/periodic-ci-openshift-multiarch-master-nightly-4.15-ocp-e2e-ovn-ppc64le-powervs']    
+    for url in url_set:
+        job_list = get_jobs(url) 
+        for job in job_list:
+            cluster_status=cluster_deploy_status(job["SpyglassLink"])
+            print(job,cluster_status)
+            if cluster_status == 'SUCCESS':
+                b,c = job_classifier(job["SpyglassLink"])
+                e2e_failures = get_failed_e2e_testcases(job["SpyglassLink"],b)
+                print(e2e_failures)
+            else:
+                print("cluster Installation failure")
+                #write function analyze cluster installation failures
+
+temporary_main_function()
