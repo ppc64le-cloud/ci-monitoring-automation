f47b9e3b193e304982c004a566ee7881f2d18a7a
SurajGudaji <Suraj.Gudaji1@ibm.com>
2023-11-09 15:11:35 +0530

    Added code to pass job info_type as a argument

--- CI_DailyBuildUpdates.py
+++ CI_DailyBuildUpdates.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib3
 import monitor
 import subprocess
+import argparse
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -11,9 +12,12 @@ with open('config.json') as config_file:
 
 
 def new_main(config_data):
-    if config_data["info_type"] == "True":
+    parser = argparse.ArgumentParser(description='Specifies if user needs brief/detailed job information')
+    parser.add_argument('--info_type', default='brief', help='specify the job info type')
+    args = parser.parse_args()
+    if args.info_type == "brief":
         monitor.get_brief_job_info(config_data)
-    elif config_data["info_type"] == False:
+    elif args.info_type == "detailed":
         print("Need to develop functions to get detailed information")
 
 new_main(config_data)
