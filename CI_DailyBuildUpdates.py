0d9364ed9ccef101db53f81163f423369eb49e72
KeerthanaAP <akeerthana1995@gmail.com>
2023-12-01 20:29:28 +0530

    Added support to get job runs based on zone/leases
    Signed-off-by: KeerthanaAP <akeerthana1995@gmail.com>

--- CI_DailyBuildUpdates.py
+++ CI_DailyBuildUpdates.py
@@ -13,16 +13,17 @@ with open('config.json') as config_file:
 
 
 def new_main(config_data):
-    parser = argparse.ArgumentParser(description='Specifies if user needs brief/detailed job information')
+    parser = argparse.ArgumentParser(description='Get the daily buid updates')
     parser.add_argument('--info_type', default='brief', help='specify the job info type')
+    parser.add_argument('--zone', help='specify the lease/zone', type= lambda arg:arg.split(','))
     args = parser.parse_args()
     if args.info_type == "brief":
         summary_list = []
         for ci_name,ci_link in config_data.items():
-            summary_list.extend(monitor.get_brief_job_info(ci_name,ci_link))
+            summary_list.extend(monitor.get_brief_job_info(ci_name,ci_link,zone=args.zone))
         print(tabulate(summary_list, headers='keys', tablefmt="pipe", stralign='left'))
     elif args.info_type == "detailed":
         for ci_name,ci_link in config_data.items():
-            monitor.get_detailed_job_info(ci_name,ci_link)
+            monitor.get_detailed_job_info(ci_name,ci_link,zone=args.zone)
 
 new_main(config_data)
